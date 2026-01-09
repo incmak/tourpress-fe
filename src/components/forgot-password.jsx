ac2bd96e344453bd4ab9bdc3806d7fab1bd50800
Mueen Ahmed <ahmedmuieen@gmail.com>
2026-01-09 12:05:15 +0530

    feat: integrate forgot and reset password

--- src/components/forgot-password.jsx
+++ src/components/forgot-password.jsx
@@ -9,13 +9,43 @@ import {
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'sonner';
+import { useMutation } from '@tanstack/react-query';
 
 export function ForgotPasswordForm({ className, ...props }) {
+  const baseURL = 'http://localhost:3000/api/v1';
+  const forgotPassword = async (body) =>
+    axios.post(`${baseURL}/users/forgot-password`, body);
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: forgotPassword,
+    onSuccess: () => {
+      toast.success('Password reset link sent on you email!', {
+        description: 'Please check your inbox.',
+      });
+    },
+    onError: (error) => {
+      console.log('🚀 ~ LoginForm ~ error:', error);
+      toast.error('Forgot password Failed.', {
+        description: error.response.data.message || 'Please try again.',
+      });
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const [email] = e.target.elements;
+
+    mutate({
+      email: email.value,
+    });
+  };
   return (
     <div className={cn('flex flex-col gap-6 w-full', className)} {...props}>
       <Card className='overflow-hidden p-0'>
         <CardContent className='grid p-0 md:grid-cols-2'>
-          <form className='p-6 md:p-8'>
+          <form className='p-6 md:p-8' onSubmit={handleSubmit}>
             <FieldGroup>
               <div className='flex flex-col items-center gap-2 text-center'>
                 <h1 className='text-2xl font-bold'>Welcome back</h1>
@@ -28,12 +58,15 @@ export function ForgotPasswordForm({ className, ...props }) {
                 <Input
                   id='email'
                   type='email'
+                  name='email'
                   placeholder='m@example.com'
                   required
                 />
               </Field>
               <Field>
-                <Button type='submit'>Continue</Button>
+                <Button type='submit' disabled={isPending}>
+                  {isPending ? 'Sending reset link...' : 'Send reset link'}
+                </Button>
               </Field>
 
               <FieldDescription className='text-center'>
--- src/components/reset-password.jsx
+++ src/components/reset-password.jsx
@@ -9,13 +9,58 @@ import {
   FieldSeparator,
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { toast } from 'sonner';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export function ResetPasswordForm({ className, ...props }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  console.log(
+    '🚀 ~ ResetPasswordForm ~ searchParams:',
+    searchParams.get('token')
+  );
+  const baseURL = 'http://localhost:3000/api/v1';
+  const resetPassword = async (body) =>
+    axios.patch(`${baseURL}/users/reset-password/${body.token}`, body);
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: resetPassword,
+    onSuccess: () => {
+      toast.success('Password reset successfully!', {
+        description: 'Redirecting to dashboard...',
+      });
+      navigate('/login');
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error('Reset password Failed.', {
+        description: error.response.data.message || 'Please try again.',
+      });
+      if (error.response.status === 400) {
+        navigate('/forgot-password');
+      }
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const [password, confirmPassword] = e.target.elements;
+
+    mutate({
+      password: password.value,
+      passwordConfirm: confirmPassword.value,
+      token: searchParams.get('token'),
+    });
+
+    // Handle form submission logic here
+  };
   return (
     <div className={cn('flex flex-col gap-6 w-full', className)} {...props}>
       <Card className='overflow-hidden p-0'>
         <CardContent className='grid p-0 md:grid-cols-2'>
-          <form className='p-6 md:p-8'>
+          <form className='p-6 md:p-8' onSubmit={handleSubmit}>
             <FieldGroup>
               <div className='flex flex-col items-center gap-2 text-center'>
                 <h1 className='text-2xl font-bold'>Reset your password</h1>
@@ -40,7 +85,9 @@ export function ResetPasswordForm({ className, ...props }) {
                 />
               </Field>
               <Field>
-                <Button type='submit'>Sign up</Button>
+                <Button type='submit' disabled={isPending}>
+                  {isPending ? 'Resetting...' : 'Reset Password'}
+                </Button>
               </Field>
 
               <FieldDescription className='text-center'>
